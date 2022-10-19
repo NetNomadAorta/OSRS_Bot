6dc09470235aa020493e9c501519f756e3f238c4
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-10-19 12:45:52 -0700

    Update Smither-VM.py

--- Smither-VM.py
+++ Smither-VM.py
@@ -13,7 +13,8 @@ SAVE_NAME_OD = "./Models/OSRS_Agility-0.model"
 DATASET_PATH = "./Training_Data/" + SAVE_NAME_OD.split("./Models/",1)[1].split("-",1)[0] +"/"
 IMAGE_SIZE              = int(re.findall(r'\d+', SAVE_NAME_OD)[-1] ) # Row and column number 
 MIN_SCORE               = 0.7
-TIME_BETWEEN_MINING     = 4 # Set 2.0 default for one pick iron
+RESOLUTION_X            = 1920
+RESOLUTION_Y            = 976
 # User Parameters/Constants to Set
 XML_DIR = "./XML_Files/"
 SLEEP_TIME_BETWEEN_TYPETHIS = 0.01
@@ -40,21 +41,22 @@ def typeThis(toType):
     time.sleep(SLEEP_TIME_BETWEEN_TYPETHIS)
 
 
-def cursor(x,y, should_scaler = True):
-    if should_scaler:
-        x = int(x*2/3)
-        y = int(y*2/3)
+def cursor(x,y):
+    x = round(x * RESOLUTION_X)
+    y = round(y * RESOLUTION_Y)
     win32api.SetCursorPos((x,y))
 
 
-def left_click(x, y, time_sleep = 0, should_scaler = False, 
-               should_rand_click = True):
-    if should_scaler:
-        x = int(x*2/3)
-        y = int(y*2/3)
+def left_click(x, y, time_sleep = 0, should_rand_click = True):
+    x = round(x * RESOLUTION_X)
+    y = round(y * RESOLUTION_Y)
+    
     if should_rand_click:
-        x = x + random.randint(-5, 5)
-        y = y + random.randint(-6, 6)
+        rand_scaler_x = round(RESOLUTION_X * 0.002)
+        rand_scaler_y = round(RESOLUTION_Y * 0.002)
+        x = x + random.randint(-rand_scaler_x, rand_scaler_x)
+        y = y + random.randint(-rand_scaler_y, rand_scaler_y)
+    
     win32api.SetCursorPos((x,y))
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
@@ -75,11 +77,11 @@ def fix_minimap():
 
 def smelter():
     # Clicks Anvil
-    left_click(500, 440, time_sleep=1.5)
+    left_click(0.495, 0.580, time_sleep=1.5)
     
     # Clicks item to smith
-    x = 500-27*0
-    y = 300-55*2
+    x = 0.49-0.055*0
+    y = 0.41-0.055*2
     left_click(x, y, time_sleep=80)
 
 
@@ -87,19 +89,19 @@ def banker():
     fix_minimap()
     
     # Runs to bank
-    left_click(893, 72, time_sleep=5, should_rand_click = False)
+    left_click(0.931, 0.072, time_sleep=5, should_rand_click = False)
     
     # Clicks bank
-    left_click(560, 360, time_sleep=1)
+    left_click(0.54, 0.45, time_sleep=1)
     
     # Deposits all
-    left_click(860, 440, time_sleep=0.5)
+    left_click(0.92, 0.67, time_sleep=0.5)
     
     # Withdraw bars
-    left_click(220, 480, time_sleep=0.5)
+    left_click(0.35, 0.495, time_sleep=0.5)
     
     # Runs to anvil
-    left_click(918, 143, time_sleep=5, should_rand_click = False)
+    left_click(0.945, 0.147, time_sleep=5, should_rand_click = False)
 
 
 
